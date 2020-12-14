09529ccd8eadb0afe33f9b54b891994922108d92
jairo <jairophg.lalala2014@gmail.com>
2020-12-14 03:15:33 -0600

    arreglando bugs inesperados de la api kill

--- Api_go/main.go
+++ Api_go/main.go
@@ -160,6 +160,9 @@ func killProcess(w http.ResponseWriter, r *http.Request){
 	//fmt.Fprintf(w, "POST para eliminar proceso")
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+    	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+    	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 	var proce procesoBorrado
 	_ = json.NewDecoder(r.Body).Decode(&proce)
 	PID := string(proce.Proceso)
